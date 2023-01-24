68142c27bc5f65cf10547b44f264bfc492834f36
Enock <enock.mokua@student.moringaschool.com>
2023-01-24 13:15:22 +0300

    create actions for scrape

--- app/controllers/scrapes_controller.rb
+++ app/controllers/scrapes_controller.rb
@@ -1,2 +1,23 @@
 class ScrapesController < ApplicationController
+  # require "open-uri"
+  # require "nokogiri"
+
+  # def scrape
+  #   doc = Nokogiri.HTMl(open(params[:scrape]))
+  # end
+
+  def index
+    render json: Scrape.all
+  end
+
+  def create
+    scrape = Scrape.create(scrape_params)
+    render json: scrape
+  end
+
+  private
+
+  def scrape_params
+    params.permit(:scrape, :pages)
+  end
 end
