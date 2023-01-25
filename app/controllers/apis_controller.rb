43f271523e2ece76fc6008a53725a0cbe5b459ae
Enock <enock.mokua@student.moringaschool.com>
2023-01-25 06:50:30 +0300

    create apis contriller  actions

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,2 +1,21 @@
 class ApisController < ApplicationController
+  def index
+    render json: Api.all
+  end
+
+  def create
+    a = Api.create(a_params)
+    render json: a
+  end
+
+  def show
+    a = Api.find(params[:id])
+    render json: a
+  end
+
+  private
+
+  def a_params
+    params.permit(:name, :link)
+  end
 end
